4813c1d0bc710fd549f94f66cedc38a2da18be18
CodFrm <code.farmer@qq.com>
2019-03-17 23:59:02 +0800

    睡觉,明天上班整

---
+++ leetcode/347.c
@@ -0,0 +1,20 @@
+//学习堆来解决这个问题
+
+struct heap {
+    int key;
+    int val;
+}
+
+int push(heap* heap,int key,int len){
+    int i = 0;
+}
+/**
+ * Return an array of size *returnSize.
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* topKFrequent(int* nums, int numsSize, int k, int* returnSize) {
+    returnSize = malloc(numsSize * sizeof(heap));
+    for (int i = 0; i < numsSize; i++) {
+        push(returnSize, key,numsSize);
+    }
+}
