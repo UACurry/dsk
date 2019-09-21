fa40992042497a1dc61bbecdb75e6c5774ac4586
CodFrm <code.farmer@qq.com>
2019-09-21 10:39:49 +0800

    n!

--- test.c
+++ test.c
@@ -8,7 +8,7 @@ int main()
     printf("Hello World!\n");
     int a = 1, b = 2, c = 3;
     int d = (a > b) && (c++);
-    printf("%d\t%d\n", c,MIN(100, 20));
+    printf("%d\t%d\n", c, MIN(100, 20));
     getchar();
     return 0;
 }
